d876e5efb8560db0595c5029e1c4b31b0b661cd9
Huy Dang Lê-Ngô <huy-dang.le-ngo@polymtl.ca>
2017-04-25 22:55:49 -0400

    Add unit tests for the LoggedIn component

---
+++ src/client/components/__tests__/LoggedIn.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Radium from 'radium';
+import { shallow } from 'enzyme';
+
+import LoggedIn from '../LoggedIn';
+import OauthRedirect from '../OauthRedirect';
+import Posts from '../Posts';
+
+describe('<LoggedIn />', () => {
+  beforeAll(() => {
+    Radium.TestMode.enable();
+  });
+
+  it('should render a OauthRedirect if the access token is not in the localStorage', () => {
+    window.localStorage = {
+      getItem: jest.fn().mockReturnValue(),
+    };
+    const wrapper = shallow(<LoggedIn appId="testApplicationId" />);
+    expect(wrapper.containsMatchingElement(<OauthRedirect />)).toBe(true);
+  });
+
+  it('should render a list of posts if the access token is in the localStorage', () => {
+    window.localStorage = {
+      getItem: jest.fn().mockReturnValue('test_access_token'),
+    };
+
+    const wrapper = shallow(<LoggedIn appId="testApplicationId" />);
+    expect(wrapper.containsMatchingElement(<Posts appId="testApplicationId" />)).toBe(true);
+  });
+});
